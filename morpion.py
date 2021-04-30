f8a626412de0dad12129f35cbb31b5587ebc84f8
crocogab <50633541+crocogab@users.noreply.github.com>
2021-04-30 12:38:23 +0200

    Update morpion.py

--- morpion.py
+++ morpion.py
@@ -1,19 +1,21 @@
 import os
 
+#gab
 grille = [["-","-","-"],
           ["-","-","-"],
           ["-","-","-"]]
 
 jeu = True
 
+#gab
 poss = [[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]]
 
 # defs
-
+#gab
 def show_grid():
     """ affiche la grille actuelle """
     print(f"{grille[0]}\n{grille[1]}\n{grille[2]}")
-
+#gab
 def num_to_pos(num):
     """ transforme le num de case donné en positions """
     return poss[num-1]
@@ -56,6 +58,14 @@ def check_win(): # aurelien
     if (grille[0][0] == grille[1][1] and grille[0][0] == grille[2][2] and grille[0][0] !="-") or (grille[0][2] == grille[1][1] and grille[0][2] == grille[2][0] and grille[0][2] !="-"):
             print(f"C'est gagné sur la diagonale pour {grille[1][1]} !\n")
             quit()
+    
+    # print(grille[0].count('X')+grille[0].count('O')+grille[1].count('X')+grille[1].count('O')+grille[2].count('X')+grille[2].count('O')) Test
+    if grille[0].count('X')+grille[0].count('O')+grille[1].count('X')+grille[1].count('O')+grille[2].count('X')+grille[2].count('O') ==9:
+        print(f"Match nul !\n")
+        quit()
+
+
+
 
 clear = lambda: os.system('cls')
 
@@ -67,15 +77,17 @@ print("les cases sont numérotées de 1 à 9, de haut en bas et des gauche à dr
 print("|1|2|3| \n|4|5|6| \n|7|8|9| \n")
 
 
-show_grid()
+show_grid()#gab
 while jeu :
 
-    # tour des croix 
+    # tour des croix / Gab
     tour_croix(num_to_pos(int(input("CROIX : dans quelle case veux-tu jouer ?\n"))))
-    #print(len(grille[0]),)
+    
+            
     check_win()
 
-    # tour des ronds
+    # tour des ronds / Gab
     tour_ronds(num_to_pos(int(input("RONDS : dans quelle case veux-tu jouer ?\n"))))
-    #print(len(grille))
+    
+            
     check_win()
